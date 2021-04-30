c73c9c4d6db3f351e71bd98d5b1c966fd586f7ea
Mariana Amina <mariana.dantasweb7@gmail.com>
2021-04-29 22:34:25 -0300

    Add a clear button and replace prompt with an input

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,34 @@
 let container = document.querySelector(".container");
+let inputAsk = document.querySelector(".input-pixels");
+let buttonClear = document.querySelector(".button-clear");
+let buttonPixels = document.querySelector(".button-pixels");
+let pixels;
 
-let promptAsk = prompt("How many squares, buddy?", "16");
-let pixels = parseInt(promptAsk);
+buttonPixels.addEventListener("click", function() {
+    container.innerHTML = " "
+    pixels = parseInt(inputAsk.value);
+    makeGrid();
+})
 
-for(let i = 0; i < pixels*pixels; i++){
-    let div = document.createElement('div');
-    div.style.width = 300/pixels + "px";
-    div.style.height = 300/pixels + "px";
-    div.addEventListener("mouseover", function() {
-        div.style.backgroundColor = "black";
-    })
-    container.appendChild(div);
+buttonClear.addEventListener("click", function() {
+    for(let i = 0; i < container.children.length; i++){
+        container.children[i].style.backgroundColor = "rgb(127, 255, 212)"; 
+    }
+    // container.innerHTML = " ";
+    // makeGrid();
+});
+
+function makeGrid(){
+    if(pixels > 100) return;
+    if(!Number.isInteger(pixels)) return;
+
+    for(let i = 0; i < pixels*pixels; i++){
+        let div = document.createElement('div');
+        div.style.width = 300/pixels + "px";
+        div.style.height = 300/pixels + "px";
+        div.addEventListener("mouseover", function() {
+            div.style.backgroundColor = "black";
+        })
+        container.appendChild(div);
+    }
 }
-
-console.log(container.children)
