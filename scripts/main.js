98d7a56913543ec82d15c4594800cd9df4e3b251
Mariana Amina <mariana.dantasweb7@gmail.com>
2021-05-03 21:02:21 -0300

    Fix bug with Clear Button

--- scripts/main.js
+++ scripts/main.js
@@ -4,21 +4,24 @@ let buttonClear = document.querySelector(".button-clear");
 let buttonPixels = document.querySelector(".button-pixels");
 let pixels;
 
+makeGrid(10);
+
 buttonPixels.addEventListener("click", function() {
     container.innerHTML = " "
     pixels = parseInt(inputAsk.value);
-    makeGrid();
+    makeGrid(pixels);
 })
 
 buttonClear.addEventListener("click", function() {
     for(let i = 0; i < container.children.length; i++){
-        container.children[i].style.backgroundColor = "rgb(127, 255, 212)"; 
+        container.children[i].setAttribute("data-cleared", "true");
+        container.children[i].style.backgroundColor = "rgb(255, 255, 255)"; 
     }
     // container.innerHTML = " ";
     // makeGrid();
 });
 
-function makeGrid(){
+function makeGrid(pixels){
     if(pixels > 100) return;
     if(!Number.isInteger(pixels)) return;
 
@@ -26,9 +29,19 @@ function makeGrid(){
         let div = document.createElement('div');
         div.style.width = 300/pixels + "px";
         div.style.height = 300/pixels + "px";
+        div.setAttribute("data-cleared", "false")
+        let opacity = 0;
+        
         div.addEventListener("mouseover", function() {
-            div.style.backgroundColor = "black";
+            if(div.getAttribute("data-cleared") === "true") {
+                opacity = 0;
+                div.setAttribute("data-cleared", "false");
+            }
+            if(opacity === 1) return;
+            opacity += 0.1;
+            div.style.backgroundColor = "rgba(0, 0, 0, "+ opacity +")";
         })
         container.appendChild(div);
+        console.dir(div);
     }
 }
